88fc577dbff0b05e187899ac3cffec081b035769
KFrodente <kianfrodente@gmail.com>
2025-08-20 09:24:46 -0600

    movement works based on plane rotation

---
+++ Assets/_Project/Movement/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform referencePlane;
+    [SerializeField] private float moveSpeed;
+    
+    private Vector2 moveDir;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = (referencePlane.right * moveDir.x * moveSpeed) + (referencePlane.forward * moveDir.y * moveSpeed);
+        Debug.Log(rb.linearVelocity);
+        Debug.Log(moveDir);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveDir = context.ReadValue<Vector2>();
+    }
+}
