dc809ca7615b101634f5b3cefb0cc74200edc7fc
Jacob Leonardo <leonardojacob257@gmail.com>
2025-09-01 16:56:29 -0600

    Network Sprite

---
+++ Assets/_Project/GameLoop/NetworkSprite.cs
@@ -0,0 +1,47 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkSprite : NetworkBehaviour
+{
+	[SerializeField] private SpriteRenderer spriteRenderer;
+	[SerializeField] private Sprite[] availableSprites;
+
+	private NetworkVariable<int> spriteIndex = new NetworkVariable<int>(
+		0,
+		NetworkVariableReadPermission.Everyone,
+		NetworkVariableWritePermission.Owner);
+
+	private void Awake()
+	{
+		if (spriteRenderer == null)
+			spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public override void OnNetworkSpawn()
+	{
+		spriteIndex.OnValueChanged += OnSpriteChanged;
+		OnSpriteChanged(0, spriteIndex.Value);
+	}
+
+	private void OnDestroy()
+	{
+		spriteIndex.OnValueChanged -= OnSpriteChanged;
+	}
+
+	private void OnSpriteChanged(int oldValue, int newValue)
+	{
+		if (newValue >= 0 && newValue < availableSprites.Length)
+		{
+			spriteRenderer.sprite = availableSprites[newValue];
+		}
+	}
+
+	[ServerRpc]
+	public void ChangeSpriteServerRpc(int newIndex)
+	{
+		if (newIndex >= 0 && newIndex < availableSprites.Length)
+		{
+			spriteIndex.Value = newIndex;
+		}
+	}
+}
