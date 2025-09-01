cb80755e883883653454f0f4c6e404e3d364773a
Chase Guymon <chase.guymon@q.com>
2025-09-01 17:00:31 -0600

    Proper User

--- Assets/_Project/Menus/LobbyUI.cs
+++ Assets/_Project/Menus/LobbyUI.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyUI : MonoBehaviour
 {
     [SerializeField] private LobbyReadyStateDisplay display;
     private LobbyUIController lobbyUIController;
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private Image playerImage;
     public void ToggleReadyState()
     {
         if(lobbyUIController.IsOwner) lobbyUIController.ToggleReadyStateServerRpc();
@@ -11,6 +14,28 @@ public class LobbyUI : MonoBehaviour
     public void Initialize(LobbyUIController controller)
     {
         lobbyUIController = controller;
+        if (lobbyUIController.IsOwner)
+        {
+            arrow.SetActive(true);
+            playerImage.sprite = GetRandomImage();
+        }
+        else
+        {
+            arrow.SetActive(false);
+            playerImage.sprite = GetOtherPlayerImage();
+        }
+    }
+
+    private Sprite GetRandomImage()
+    {
+        var faces = Resources.LoadAll<Sprite>("PlayerImages");
+        if(faces.Length <= 0) return GetOtherPlayerImage();
+        return faces[Random.Range(0, faces.Length)];
+    }
+
+    private Sprite GetOtherPlayerImage()
+    {
+        return Resources.Load<Sprite>("DefaultImage");
     }
 
     public void SetReady()
